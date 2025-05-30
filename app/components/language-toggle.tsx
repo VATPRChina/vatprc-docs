42775921a5627c9c4ebfb2f8e1eceb6abd35ba90
Yuze Fu <i@xfox.me>
2025-05-31 04:52:06 +0900

    fix: translation switch

--- app/components/language-toggle.tsx
+++ app/components/language-toggle.tsx
@@ -1,11 +1,14 @@
 import { Button } from "./ui/button";
 import { getLocale } from "@/lib/i18n/runtime";
 import { getPathname } from "@/lib/utils";
+import { useRouterState } from "@tanstack/react-router";
 import { TbLanguage } from "react-icons/tb";
 
 export const LanguageToggle = () => {
+  const state = useRouterState();
+  const pathname = state.location.pathname ?? getPathname();
   const pathnameAnotherLang =
-    getLocale() === "zh-cn" ? getPathname().replace("/zh-cn", "/en") : getPathname().replace("/en", "/zh-cn");
+    getLocale() === "zh-cn" ? pathname.replace("/zh-cn", "/en") : pathname.replace("/en", "/zh-cn");
 
   return (
     <Button variant="ghost" size="icon" asChild>
