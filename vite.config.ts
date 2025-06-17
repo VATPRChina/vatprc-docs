8f7991833280f64afc1de780dd8606a116676de9
Yuze Fu <i@xfox.me>
2025-06-18 00:59:31 +0900

    feat: devinxi for tanstack start

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart(),
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./app/lib/i18n",
+      strategy: ["url", "baseLocale"],
+      urlPatterns: [
+        {
+          pattern: "/:path(.*)?",
+          localized: [
+            ["zh-cn", "/zh-cn/:path(.*)?"],
+            ["en", "/en/:path(.*)?"],
+          ],
+        },
+      ],
+    }),
+    sentryVitePlugin({
+      org: "xfoxfu",
+      project: "vatprc-homepage",
+    }),
+  ],
+  build: { sourcemap: true },
+  server: {
+    proxy: {
+      "/api/cors/vatsim-events-prc": { target: "https://my.vatsim.net/api/v2/events/latest", changeOrigin: true },
+      "/uniapi": {
+        target: "https://uniapi.vatprc.net",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/uniapi/, ""),
+      },
+    },
+  },
+});
