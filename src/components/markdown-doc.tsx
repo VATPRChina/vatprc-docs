a91799d32b328d1f3d452fb0bd0b78c41cc153d5
Yuze Fu <i@xfox.me>
2025-11-22 14:22:32 +0900

    feat: improve toc

--- src/components/markdown-doc.tsx
+++ src/components/markdown-doc.tsx
@@ -1,58 +1,30 @@
-import type { Toc, TocEntry } from "@stefanprobst/remark-extract-toc";
+import { TableOfContents } from "@mantine/core";
 import type React from "react";
-import { Fragment } from "react";
-
-export const TableOfContents: React.FC<{ tableOfContents: Toc; maxDepth?: number }> = ({
-  tableOfContents,
-  maxDepth,
-}) => {
-  if (!maxDepth || maxDepth <= 0) {
-    return null;
-  }
-
-  if (tableOfContents?.[0]?.depth === 1) {
-    return (
-      <>
-        {tableOfContents.map((toc: TocEntry) => (
-          <Fragment key={toc.value}>
-            <a href={`#${toc.id}`} className="font-normal no-underline">
-              {toc.value}
-            </a>
-            <br />
-            {toc.children && <TableOfContents tableOfContents={toc.children} maxDepth={maxDepth - 1} />}
-          </Fragment>
-        ))}
-      </>
-    );
-  }
-
-  return (
-    <ul>
-      {tableOfContents.map((toc: TocEntry) => (
-        <li key={toc.value}>
-          <a href={`#${toc.id}`} className="font-normal no-underline">
-            {toc.value}
-          </a>
-          {toc.children && <TableOfContents tableOfContents={toc.children} maxDepth={maxDepth - 1} />}
-        </li>
-      ))}
-    </ul>
-  );
-};
 
 export const MarkdownDoc: React.FC<{
   children: React.ReactNode;
-  toc: Toc;
   tocHeader?: React.ReactNode;
-}> = ({ children, toc, tocHeader }) => {
+}> = ({ children, tocHeader }) => {
   return (
     <div className="container mx-auto flex flex-col md:flex-row">
       <div className="px-4 py-6 md:flex-2/3 md:px-12">
-        <article className="prose dark:prose-invert vatprc-prose mx-auto">{children}</article>
+        <article className="prose dark:prose-invert vatprc-prose mx-auto" id="markdown-doc">
+          {children}
+        </article>
       </div>
-      <div className="prose dark:prose-invert prose-ul:my-0 prose-li:my-0 z-10 md:sticky md:top-24 md:max-h-dvh md:flex-1/3 md:overflow-y-scroll">
+      <div className="z-10 md:sticky md:top-24 md:max-h-dvh md:flex-1/3 md:overflow-y-scroll">
         {tocHeader}
-        <TableOfContents tableOfContents={toc} maxDepth={3} />
+        <TableOfContents
+          variant="light"
+          scrollSpyOptions={{
+            selector: "#markdown-doc :is(h1, h2, h3, h4, h5, h6)",
+          }}
+          getControlProps={({ data }) => ({
+            component: "a",
+            href: data.getNode().getAttribute("id") && "#" + data.getNode().getAttribute("id"),
+            children: data.value,
+          })}
+        />
       </div>
     </div>
   );
