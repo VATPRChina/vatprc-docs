c1bd8fa446a072bf7c7d72f7888e0c2416837f5e
Yuze Fu <i@xfox.me>
2026-01-03 12:16:16 +0900

    fix: language toggle

--- src/components/app/language-toggle.tsx
+++ src/components/app/language-toggle.tsx
@@ -1,4 +1,4 @@
-import { wrapPromiseWithLog } from "@/lib/utils";
+import { getLocalPathname } from "@/lib/i18n";
 import { useLingui } from "@lingui/react/macro";
 import { ActionIcon } from "@mantine/core";
 import { TbLanguage } from "react-icons/tb";
@@ -9,11 +9,10 @@ export const LanguageToggle = () => {
   const { i18n, t } = useLingui();
   const nextLang = i18n.locale === "zh-cn" ? "en" : "zh-cn";
 
-  const onClick = wrapPromiseWithLog(async () => {
-    await cookieStore.set(LANGUAGE_STORAGE_KEY, nextLang);
-    localStorage.removeItem(LANGUAGE_STORAGE_KEY);
-    i18n.activate(nextLang);
-  });
+  const onClick = () => {
+    localStorage.setItem("vatprc-homepage-locale", nextLang);
+    window.location.assign(getLocalPathname(nextLang));
+  };
 
   const ariaLabel = nextLang === "en" ? t`Switch to English` : t`Switch to Chinese`;
 
