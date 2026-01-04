c8b8137a6c1faebb40b8195caed42aeefd8a7e18
Yuze Fu <i@xfox.me>
2026-01-04 12:07:41 +0900

    fix: atc permission unset

--- src/components/atc-permission-modal.tsx
+++ src/components/atc-permission-modal.tsx
@@ -1,16 +1,17 @@
 import { components } from "@/lib/api";
 import { $api } from "@/lib/client";
 import { localizeWithMap } from "@/lib/i18n";
-import { promiseWithLog } from "@/lib/utils";
+import { promiseWithLog, wrapPromiseWithLog } from "@/lib/utils";
 import { utc } from "@date-fns/utc";
 import { MessageDescriptor } from "@lingui/core";
 import { msg } from "@lingui/core/macro";
 import { Trans, useLingui } from "@lingui/react/macro";
 import { ActionIcon, Group, Modal, Select, Stack } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
+import { useForm } from "@tanstack/react-form";
 import { useQueryClient } from "@tanstack/react-query";
-import { endOfDay, format, parse, parseISO } from "date-fns";
-import { ComponentProps, FC, MouseEventHandler, useEffect, useState } from "react";
+import { endOfDay, format, parse } from "date-fns";
+import { ComponentProps, FC, FormEventHandler } from "react";
 import { TbDeviceFloppy } from "react-icons/tb";
 
 interface AtcPermissionModalProps extends ComponentProps<typeof Modal> {
@@ -62,69 +63,84 @@ const PositionKindView: FC<{ userId: string; kindId: string; kindName: MessageDe
     { onSuccess: invalidateQueries },
   );
 
-  const [state, setState] = useState<components["schemas"]["UserControllerState"] | null>(permission?.state ?? null);
-  const [soloExpiresAt, setSoloExpiresAt] = useState<Date | null>(
-    permission?.solo_expires_at ? new Date(permission.solo_expires_at) : null,
-  );
+  const form = useForm({
+    defaultValues: {
+      state: permission?.state ?? null,
+      solo_expires_at: permission?.solo_expires_at ?? null,
+    } as {
+      state: components["schemas"]["UserControllerState"] | null;
+      solo_expires_at: string | null;
+    },
+    onSubmit: ({ value: { state, solo_expires_at } }) => {
+      if (state !== null) {
+        put({
+          params: { path: { id: userId, kind: kindId } },
+          body: { state, solo_expires_at },
+        });
+      } else {
+        del({ params: { path: { id: userId, kind: kindId } } });
+      }
+    },
+  });
 
-  useEffect(() => {
-    if (!state) setState(permission?.state ?? null);
-    if (!soloExpiresAt) setSoloExpiresAt(permission?.solo_expires_at ? parseISO(permission.solo_expires_at) : null);
-  }, [permission, soloExpiresAt, state]);
-
-  const onSave: MouseEventHandler = () => {
-    if (state !== null) {
-      put({
-        params: { path: { id: userId, kind: kindId } },
-        body: {
-          state: state,
-          solo_expires_at: soloExpiresAt ? soloExpiresAt.toISOString() : null,
-        },
-      });
-    } else {
-      del({ params: { path: { id: userId, kind: kindId } } });
-    }
+  const onSave: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    wrapPromiseWithLog(form.handleSubmit());
   };
 
   return (
-    <Group align="end">
-      <Select
-        label={i18n._(kindName)}
-        data={POSITION_STATE_MAP.entries()
-          .map(([id, name]) => ({ value: id, label: i18n._(name) }))
-          .toArray()}
-        value={state}
-        placeholder={t`No permission`}
-        clearable
-        className="flex-1"
-        onChange={(v) => setState(v as components["schemas"]["UserControllerState"] | null)}
-        disabled={isPending || isPutPending || isDelPending}
-        description={t`Current Value: ` + localizeWithMap(POSITION_STATE_MAP, permission?.state ?? "", i18n)}
-      />
-      {state === "solo" && (
-        <DateInput
-          label={t`Solo Expiration Date`}
-          value={soloExpiresAt && format(soloExpiresAt, "yyyy-MM-dd", { in: utc })}
-          onChange={(e) =>
-            setSoloExpiresAt(e !== null ? endOfDay(parse(e, "yyyy-MM-dd", Date.now(), { in: utc }), { in: utc }) : null)
+    <form onSubmit={onSave}>
+      <Group align="end">
+        <form.Field name="state">
+          {(field) => (
+            <Select
+              label={i18n._(kindName)}
+              data={POSITION_STATE_MAP.entries()
+                .map(([id, name]) => ({ value: id, label: i18n._(name) }))
+                .toArray()}
+              value={field.state.value}
+              placeholder={t`No permission`}
+              clearable
+              className="flex-1"
+              onChange={(value) => field.handleChange(value as components["schemas"]["UserControllerState"] | null)}
+              onBlur={field.handleBlur}
+              disabled={isPending || isPutPending || isDelPending}
+              description={t`Current Value: ` + localizeWithMap(POSITION_STATE_MAP, permission?.state ?? "", i18n)}
+            />
+          )}
+        </form.Field>
+        <form.Subscribe selector={(state) => state.values.state}>
+          {(value) =>
+            value === "solo" && (
+              <form.Field name="solo_expires_at">
+                {(field) => (
+                  <DateInput
+                    label={t`Solo Expiration Date`}
+                    value={field.state.value && format(field.state.value, "yyyy-MM-dd", { in: utc })}
+                    onChange={(v) =>
+                      field.handleChange(
+                        v !== null
+                          ? endOfDay(parse(v, "yyyy-MM-dd", Date.now(), { in: utc }), { in: utc }).toISOString()
+                          : null,
+                      )
+                    }
+                    valueFormat="YYYY-MM-DD"
+                    clearable
+                    className="flex-1"
+                    description={t`Current Value: ` + permission?.solo_expires_at}
+                    disabled={isPending || isPutPending || isDelPending}
+                  />
+                )}
+              </form.Field>
+            )
           }
-          valueFormat="YYYY-MM-DD"
-          clearable
-          className="flex-1"
-          description={t`Current Value: ` + permission?.solo_expires_at}
-          disabled={isPending || isPutPending || isDelPending}
-        />
-      )}
-      <ActionIcon
-        variant="subtle"
-        size="input-sm"
-        onClick={onSave}
-        loading={isPutPending || isDelPending}
-        disabled={state === "solo" && !soloExpiresAt}
-      >
-        <TbDeviceFloppy />
-      </ActionIcon>
-    </Group>
+        </form.Subscribe>
+        <ActionIcon variant="subtle" type="submit" size="input-sm" loading={isPutPending || isDelPending}>
+          <TbDeviceFloppy />
+        </ActionIcon>
+      </Group>
+    </form>
   );
 };
 
